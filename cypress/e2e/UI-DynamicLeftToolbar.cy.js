35d58e44779ba36936a29b5fe6263b4309d54a94
B Rahul <rahul.b@lysto.io>
2023-11-08 15:11:50 +0530

    Updated toolbar test cases

--- cypress/e2e/UI-DynamicLeftToolbar.cy.js
+++ cypress/e2e/UI-DynamicLeftToolbar.cy.js
@@ -5,27 +5,27 @@ describe('Dynamic Left Toolbar', () => {
     });
     it('Verify tournament icon and text is visible in toolbar', () => {
         cy.visit('https://lysto.gg/tournaments/upcoming')
-        cy.get('Img[src="/static/images/ic_tournament.svg"]').should('be.visible')
+        cy.get('Img[src="/static/images/ic_tournament_1_enabled.svg"]').should('be.visible')
         cy.get('#Tournament_id').should('have.text','Tournaments')
     });
     it('Verify scrims icon and text is visible in toolbar', () => {
         cy.visit('https://lysto.gg/tournaments/upcoming')
-        cy.get('Img[src="/static/images/igl.png"]').should('be.visible')
+        cy.get('Img[src="/static/images/ic_mountain_flag.svg"]').should('be.visible')
         cy.get('#Scrim_id').should('have.text','Scrims')
     });
     it('Verify contests icon and text is visible in toolbar', () => {
         cy.visit('https://lysto.gg/tournaments/upcoming')
-        cy.get('Img[src="/static/images/celebration.svg"]').should('be.visible')
+        cy.get('Img[src="/static/images/ic_badge.svg"]').should('be.visible')
         cy.get('#Quests_id').should('have.text','Contests')
     });
     it('Verify communities icon and text is visible in toolbar', () => {
         cy.visit('https://lysto.gg/tournaments/upcoming')
-        cy.get('Img[src="/static/images/ic_community.svg"]').should('be.visible')
+        cy.get('Img[src="/static/images/ic_groups_1.svg"]').should('be.visible')
         cy.get('#Communities_id').should('have.text','Communities')
     });
     it('Verify activities icon and text is visible in toolbar', () => {
         cy.visit('https://lysto.gg/tournaments/upcoming')
-        cy.get('Img[src="/static/images/momentIcon.svg"]').should('be.visible')
+        cy.get('Img[src="/static/images/ic_activities.svg"]').should('be.visible')
         cy.get('#Moments_id').should('have.text','Activities')
     });
 });
